b73706fb0006bfeef68569365ab40e443e9ce494
Mike Doel <mike@mikedoel.com>
2020-01-30 14:56:44 -0500

    Simple sinatra example from cuke repo

---
+++ sinatra/features/support/env.rb
@@ -0,0 +1,9 @@
+# See http://wiki.github.com/cucumber/cucumber/sinatra
+# for more details about Sinatra with Cucumber
+
+require File.dirname(__FILE__) + '/../../app'
+
+require 'rack/test'
+require 'capybara/cucumber'
+
+Capybara.app = Sinatra::Application
