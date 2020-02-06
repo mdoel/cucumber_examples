72d1e431176dd88e86eb170411b84c6adea4461c
Mike Doel <mike@mikedoel.com>
2020-02-06 09:14:10 -0500

    start a test server for the full process

--- full/features/support/env.rb
+++ full/features/support/env.rb
@@ -13,5 +13,11 @@ require "selenium/webdriver"
 Before('@js') do
   Capybara.run_server = false
   Capybara.current_driver = :selenium_chrome
-  Capybara.app_host = 'http://localhost:9292'
+  Capybara.app_host = 'http://localhost:3000'
+  @process = ChildProcess.build('rackup', '-p', '3000')
+  @process.start
+end
+
+After('@js') do
+  @process.stop
 end
